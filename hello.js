7590388ccc0eb12a4bd3ba610874b22db62df12f
it4smartbiz <rr@it4smartbiz.com>
2024-05-21 17:27:37 +0200

    commit

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from Cyprus i'm remotely work !";
 console.log(hello());
 exports.hello = hello;
 
